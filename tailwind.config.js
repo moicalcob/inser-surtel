ddc176540ae49e24ac2096f254ea61e6f0fb9e3d
Moisés Calzado <moisescalzado1227@gmail.com>
2021-07-08 19:17:03 +0200

    Continue working on upload's page design

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,16 @@ module.exports = {
     },
     darkMode: 'class', // or 'media' or 'class'
     theme: {
+      minWidth: {
+        '4': '10vw'
+      },
+      borderColor: theme => ({
+        ...theme('colors'),
+         DEFAULT: theme('colors.gray.300', 'currentColor'),
+        'primary': '#276092',
+        'secondary': '#ffed4a',
+        'danger': '#e3342f',
+      }),
       extend: {},
     },
     variants: {
